0bd38669605a1d328b2230d6210fbe584264a04a
vigneshpy <vickypotter2516@gmail.com>
2022-07-09 02:42:05 +0530

    fix:added trending gif section

--- src/components/Sections/TrendingSection.js
+++ src/components/Sections/TrendingSection.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../Card";
 import Box from "@mui/material/Box";
 import { Typography } from "@mui/material";
 import { Container } from "@mui/system";
 import Grid from "@mui/material/Grid";
-
+import ItemsCarousel from "react-items-carousel";
+import Icon from "@mui/material/Icon";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 const TrendingSection = () => {
+  const [activeItemIndex, setActiveItemIndex] = useState(0);
+
   return (
     <Box>
       <Typography
@@ -16,20 +21,22 @@ const TrendingSection = () => {
       </Typography>
       <Container>
         <Box sx={{ flexGrow: 1 }}>
-          <Grid container spacing={2}>
-            <Grid item xs={3}>
-              <Card />
-            </Grid>
-            <Grid item xs={3}>
-              <Card />
-            </Grid>
-            <Grid item xs={3}>
-              <Card />
-            </Grid>
-            <Grid item xs={3}>
-              <Card />
-            </Grid>
-          </Grid>
+          <ItemsCarousel
+            requestToChangeActive={setActiveItemIndex}
+            activeItemIndex={activeItemIndex}
+            numberOfCards={4}
+            gutter={20}
+            leftChevron={<ChevronLeftIcon />}
+            rightChevron={<ChevronRightIcon />}
+            outsideChevron
+            chevronWidth={40}
+          >
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+          </ItemsCarousel>
         </Box>
       </Container>
     </Box>
